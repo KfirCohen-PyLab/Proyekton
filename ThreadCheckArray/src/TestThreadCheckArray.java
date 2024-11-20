7f3e5f2b287c6a987d6d6b8b8fff23c7dd538ad3
ShereenOw <shereenowida13020@gmail.com>
2024-11-20 11:04:57 +0200

    print the winner thread

--- ThreadCheckArray/src/TestThreadCheckArray.java
+++ ThreadCheckArray/src/TestThreadCheckArray.java
@@ -1,74 +1,80 @@
 import java.util.Scanner;
 
 public class TestThreadCheckArray {
-    public static void main(String[] args) {
-        try (Scanner input = new Scanner(System.in)) {
-            Thread thread1, thread2;
-            System.out.println("Enter array size");
-            int num = input.nextInt();
-            int[] array = new int[num];
-            System.out.println("Enter numbers for array");
+	public static void main(String[] args) {
+		try (Scanner input = new Scanner(System.in)) {
+			Thread thread1, thread2;
+			System.out.println("Enter array size");
+			int num = input.nextInt();
+			int[] array = new int[num];
+			System.out.println("Enter numbers for array");
 
-            for (int index = 0; index < num; index++)
-                array[index] = input.nextInt();
+			for (int index = 0; index < num; index++)
+				array[index] = input.nextInt();
 
-            System.out.println("Enter number");
-            num = input.nextInt();
+			System.out.println("Enter number");
+			num = input.nextInt();
 
-            SharedData sd = new SharedData(array, num);
+			SharedData sd = new SharedData(array, num);
 
-            thread1 = new Thread(new ThreadCheckArray(sd), "thread1");
-            thread2 = new Thread(new ThreadCheckArray(sd), "thread2");
+			thread1 = new Thread(new ThreadCheckArray(sd), "thread1");
+			thread2 = new Thread(new ThreadCheckArray(sd), "thread2");
 
-            // Start measuring time
-            long startTime = System.currentTimeMillis();
+			// Start measuring time
+			long startTime = System.currentTimeMillis();
 
-            thread1.start();
-            thread2.start();
-            try {
-                thread1.join();
-                thread2.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+			thread1.start();
+			thread2.start();
+			try {
+				thread1.join();
+				thread2.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 
-            // End measuring time
-            long endTime = System.currentTimeMillis();
-            System.out.println( endTime + " milliseconds");
-            long totalTime = endTime - startTime;
-            System.out.println("Total running time of the threads: " + totalTime + " milliseconds");
+			// End measuring time
+			long endTime = System.currentTimeMillis();
+			System.out.println(endTime + " milliseconds");
+			long totalTime = endTime - startTime;
+			System.out.println("Total running time of the threads: " + totalTime + " milliseconds");
 
-            if (!sd.getFlag()) {
-                System.out.println("Sorry");
-                return;
-            }
-            System.out.println("Solution for b: " + sd.getB() + ", n = " + sd.getArray().length);
-            System.out.print("I:    ");
-            for (int index = 0; index < sd.getArray().length; index++)
-                System.out.print(index + "    ");
-            System.out.println();
-            System.out.print("A:    ");
-            for (int index : sd.getArray()) {
-                System.out.print(index);
-                int counter = 5;
-                while (true) {
-                    index = index / 10;
-                    counter--;
-                    if (index == 0)
-                        break;
-                }
-                for (int i = 0; i < counter; i++)
-                    System.out.print(" ");
-            }
+			if (!sd.getFlag()) {
+				System.out.println("Sorry");
+				return;
+			}
+			System.out.println("Solution for b: " + sd.getB() + ", n = " + sd.getArray().length);
+			System.out.print("I:    ");
+			for (int index = 0; index < sd.getArray().length; index++)
+				System.out.print(index + "    ");
+			System.out.println();
+			System.out.print("A:    ");
+			for (int index : sd.getArray()) {
+				System.out.print(index);
+				int counter = 5;
+				while (true) {
+					index = index / 10;
+					counter--;
+					if (index == 0)
+						break;
+				}
+				for (int i = 0; i < counter; i++)
+					System.out.print(" ");
+			}
 
-            System.out.println();
-            System.out.print("C:    ");
-            for (boolean index : sd.getWinArray()) {
-                if (index)
-                    System.out.print("1    ");
-                else
-                    System.out.print("0    ");
-            }
-        }
-    }
+			System.out.println();
+			System.out.print("C:    ");
+			for (boolean index : sd.getWinArray()) {
+				if (index)
+					System.out.print("1    ");
+				else
+					System.out.print("0    ");
+			}
+
+			if (sd.getFlag()) {
+				System.out.println("winner thread : " + sd.getWinnerThread()); // print the winner thread
+			} else {
+				System.out.println("there is no solution"); // there wasnt solution
+			}
+		}
+	}
 }
